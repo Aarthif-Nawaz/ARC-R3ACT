5bd989b5521720d8e6893895f69916605b9f924f
ridmi98 <ridmi.2018182@iit.ac.lk>
2020-03-01 11:00:07 +0530

    feat:modified Cluster_reviews.py

--- Cluster_reviews.py
+++ Cluster_reviews.py
@@ -1,16 +1,23 @@
+#Author - N.W.R.Amasha
+#Last modified Date - 3/1/2019 10:49 A.M
+
 import pandas as pd
 import csv
+from preprocessing import text_preprocessing
 
+#get reviews of the cluster 'bug_fix' from results.csv
 def clustered_bug_fixes():
     corpus=[]
     with open('results.csv',encoding='utf-8') as file:
        reader = csv.reader(file)
        for row in reader:
            for column in row:
+
                if column == "Bug_fix":
                    corpus.append(row[1])
     return corpus
 
+#get reviews of the cluster 'bug_fix' from results.csv
 def clustered_feature_reqs():
     corpus = []
     with open('results.csv', encoding='utf-8') as file:
@@ -18,6 +25,6 @@ def clustered_feature_reqs():
         for row in reader:
             for column in row:
                 if column == "Feature Request":
-                    corpus.append(row[1])
+                    corpus.append(text_preprocessing(row[1]))
     return corpus
 
