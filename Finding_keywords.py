d08cccf6d1a3bdd44f47583e14d104ad83dea744
ridmi98 <ridmi.2018182@iit.ac.lk>
2020-03-01 10:58:30 +0530

    modified:text preprocessing must be added

--- Finding_keywords.py
+++ Finding_keywords.py
@@ -1,17 +1,18 @@
-# Simple feature extraction with tfidfvectorizer
-# output - gives the keywords with the preprocessing of stopwords only
+#Author - N.W.R.Amasha
+#Last modified Date - 3/1/2019 10:49 A.M
+
+# Finding keywords with feature extraction
+# output - gives the keywords of each categorized reviews
+
 from sklearn.feature_extraction.text import TfidfVectorizer
-from Cluster_reviews import clustered_feature_reqs
-from nltk.corpus import stopwords
+from Cluster_reviews import clustered_feature_reqs, clustered_bug_fixes
+from pandas.core.frame import DataFrame
+import pandas as pd
 
+# gets reviews in an aray
+corpus = clustered_bug_fixes()
 
-#gets reviews in an aray
-corpus = clustered_feature_reqs()
-
-_stopwords = set(stopwords.words('english'))
-
-
-# get the feature names and tf-idf score of top n items
+#get the topn items' feature names and tf-idf score
 def extract_topn_features_from_vector(features, sorted_features, topn=10):
     # use only topn items from vector
     sorted_features = sorted_features[:topn]
@@ -29,7 +30,6 @@ def extract_topn_features_from_vector(features, sorted_features, topn=10):
     results = {}
     for index in range(len(ftr_values)):
         results[ftr_values[index]] = scored_values[index]
-
     return results
 
 
@@ -37,38 +37,50 @@ def sort_matrix(matrix):
     tuples = zip(matrix.col, matrix.data)
     return sorted(tuples, key=lambda x: (x[1], x[0]), reverse=True)
 
-vectorizer = TfidfVectorizer(max_df=0.85, stop_words=_stopwords,max_features=10000)
+
+vectorizer = TfidfVectorizer(max_df=0.85, max_features=10000)
 tf_idf_vector = vectorizer.fit_transform(corpus)
 features = vectorizer.get_feature_names()
 
+review = []
+key_list = []
 for n in range(len(corpus)):
 
-    # get the document that we want to extract keywords from
+    #extract keywords from the document we want
     doc = corpus[n]
 
-    # generate tf-idf for the given document
+    # give tf-idf value to the given document
     tf_idf_vector = vectorizer.transform([doc])
 
-    # sort the tf-idf vectors by descending order of scores
+    #sort the tf-idf vector in descending order according to the scores
     sorted_features = sort_matrix(tf_idf_vector.tocoo())
 
-    # extract only the top n; n here is 10
-    keywords = extract_topn_features_from_vector(features, sorted_features, 20)
+    # take out the only the topn features; here n=10
+    keywords = extract_topn_features_from_vector(features, sorted_features, 10)
 
     print("\n===Review======")
     print(doc)
+
+    review.append(doc)
+    df = pd.DataFrame(review, columns=['Reviews'])
+
     print("\n===Keywords===")
     words = []
     for k in keywords:
         words.append(k)
 
-    #sort the keywords in order of the given line in the corpus
+    # sort the keywords in order of the given line in the corpus
     new_list = sorted(words, key=corpus[n].find)
-
+    keyword_list = []
     for i in new_list:
-       print(i)
+        print(i) #prints the keywords
+        keyword_list.append(i)
+    key_list.append(keyword_list)
 
+df['KeyWords'] = key_list
 
+df.to_csv("features_result.csv")
+print("check features_result.csv for clusters")
 
 
 
