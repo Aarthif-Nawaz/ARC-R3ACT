9be01c5e2fc0f23fcebdc80f5810d78b09f34064
ridmi98 <ridmi.2018182@iit.ac.lk>
2020-03-15 21:36:53 +0530

    modified navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from 'react-bootstrap/Navbar';
-import { slide as Menu } from 'react-burger-menu';
+import { elastic as Menu } from 'react-burger-menu';
 import '../App.css';
 
 function NavBar() {
@@ -16,13 +16,17 @@ function NavBar() {
           style={{margin:20,position:"absolute",zIndex:1000}}
         />
       </Navbar.Brand>
-      <Menu>
-        <a id="home" className="menu-item" href="/">Home</a>
-        <a id="about" className="menu-item" href="/about">About</a>
-        <a id="contact" className="menu-item" href="/contact">Contact</a>
-        <a id="contact" className="menu-item" href="/support">Support</a>
+      <div id="outer-container">
+      <Menu pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" }  right>
+       
+              <a id="home" className="menu-item" href="/">Home</a>
+              <a id="about" className="menu-item" href="/about">About</a>
+              <a id="contact" className="menu-item" href="/contact">Contact</a>
+              <a id="contact" className="menu-item" href="/support">Support</a>
         
       </Menu>
+      </div>
+      
       
     </Navbar>
   );
