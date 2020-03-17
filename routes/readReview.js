f017468dcd2be1a64dc2466613b0d072c6de361f
Aarthif Nawaz <58284284+Aarthif-Nawaz@users.noreply.github.com>
2020-03-17 22:31:46 +0530

    Add files via upload
    This file is used to pause the asynchronous call from the database while retrieving user input

---
+++ routes/readReview.js
@@ -0,0 +1,34 @@
+function doSomething() {
+
+      // callback sets the received data to a global var
+  function callBack(d) {
+      window.data = d;
+  }
+      // start the async
+  myAsynchronousCall(param1, parma2, callBack);
+
+}
+
+  // start the function
+doSomething();
+
+  // make sure the global is clear
+window.data = null
+
+  // start polling at an interval until the data is found at the global
+var intvl = setInterval(function() {
+    if (window.data) { 
+        clearInterval(intvl);
+        console.log(data);
+    }
+}, 100);
+
+async function myAsynchronousCall(client,newMany, callBack){
+    
+    const result = await client.db("arc").collection("MobileAppReviews").insertMany(newListings);
+
+  console.log(`${result.insertedCount} new listing(s) created with the following id(s):`);
+  console.log(result.insertedIds);
+ 
+
+}
