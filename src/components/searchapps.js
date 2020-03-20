8de128f8715a9b3eec49074ed170565d0c2d2d7f
sajani-sihara <53464528+sajani-sihara@users.noreply.github.com>
2020-03-20 12:30:35 +0530

    Made style changes to Search for Apps page

--- src/components/searchapps.js
+++ src/components/searchapps.js
@@ -10,23 +10,26 @@ function SearchApps() {
         <div className="video-container">
           <video className="videoBg" autoPlay loop muted>
             <source
-              src={process.env.PUBLIC_URL + "/images/bgvideo2.mp4"}
+              src={process.env.PUBLIC_URL + "/images/bgvideo.mp4"}
               type="video/mp4"
             />
           </video>
           <div className="content">
             <p>We analyse mobile app reviews.</p>
-            <div>
+        
+            <div className="searchBar">
               <InputGroup className="mb-3">
                 <FormControl
                   placeholder="Search for a mobile app"
                   aria-label="search for a mobile app"
+                  className="searchPlaceholder"
                 />
                 <InputGroup.Append>
-                  <Button variant="outline-secondary">SEARCH</Button>
+                  <Button className="button" variant="outline-secondary">SEARCH</Button>
                 </InputGroup.Append>
               </InputGroup>
             </div>
+            
           </div>
         </div>
      
