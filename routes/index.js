583269ce25227f0078cbe64c09855078f99432a9
Shiromi <basilanita@gmail.com>
2020-03-22 23:38:43 +0530

    Updated to mongoDB Atlas using MongoClient

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,37 @@
-var express = require('express');
-var router = express.Router();
-const mongoose = require("mongoose");
+/* 
+This file is main start script.
+Author: Shiromi Thevarajan
+IIT ID: 2018117
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express'});
+Dependencies: express, mongoose
+*/
+
+var express = require("express");
+var router = express.Router();
+const MongoClient = require("mongodb").MongoClient;
+
+const uri =
+  "mongodb+srv://User:1234@r3act-rludw.mongodb.net/test?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
-mongoose  // connection to the mongodb
-    .connect("mongodb://localhost:27017/arc", { useNewUrlParser: true, useUnifiedTopology: true})
-    .then(()=> console.log("Connected to database."))
-    .catch(err => console.log("Error has occured: ", err))
+var collection;
+
+client.connect(err => {
+  if (err) {
+    console.log("Error has occured while connecting to database: ", err);
+  }
+  collection = client.db("Safiyyah_ARC").collection("Reviews");
+  console.log("Connected to database.");
+  // client.close();
+});
+
+/* GET home page. */
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
+});
 
 module.exports = router;
