4744f091ad243dddcd4d6d583fdc4a296eba854d
Shiromi <basilanita@gmail.com>
2020-03-24 21:59:14 +0530

    Fixed 'No Access-Control-Allow-Origin’

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require("cors");
 
 var indexRouter = require('./routes/index');
 var databaseRouter = require('./routes/database');
@@ -23,6 +24,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
