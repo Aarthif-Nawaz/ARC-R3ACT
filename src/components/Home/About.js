4ca16e6a4fde557ef12a0e867f89a87d5e12d3da
sajani-sihara <53464528+sajani-sihara@users.noreply.github.com>
2020-04-04 18:23:50 +0530

    Modified the content of About.js

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -6,7 +6,7 @@ function About() {
     <Container fluid>
       <div className="descrip-1">
         <h3 style={{ textAlign: "center", fontSize: "2vw", fontWeight: 900 }}>
-          Application  Review  Classifier
+          Application Review Classifier
         </h3>
         <p
           style={{ paddingLeft: 250, paddingRight: 250, paddingBlockStart: 30 }}
@@ -29,8 +29,26 @@ function About() {
       </div>
 
       <div className="descrip-1">
-        <h3 style={{ textAlign: "center" }}>How to use ARC?</h3>
-        <p></p>
+        <h3 style={{ textAlign: "center", fontSize: "2vw", fontWeight: 900 }}>
+          How to use ARC?
+        </h3>
+        <p
+          style={{
+            paddingLeft: 250,
+            paddingRight: 250,
+            paddingBlockStart: 30,
+            textAlign: "justify",
+          }}
+        >
+          You can see all the postive and negative reviews regarding your app
+          using ARC. All you have to do to start the process is type the name of
+          the mobile application in the search bar at the top of this page. Once
+          the app has been selected, click on Search. Give our servers a moment
+          to analyse the thousands of reviews submitted to Google Play Store
+          regarding that app. Once the analysis is over, you will be lead to a
+          menu where you can choose from three options - bug fixes, feature
+          requests and overall sentiment.
+        </p>
       </div>
 
       <div class="bgimg-2">
@@ -38,7 +56,7 @@ function About() {
           <span
             class="border"
             style={{
-              backgroundColor: "transparent",
+              backgroundColor: "#282e34",
               fontSize: "2vw",
               color: "#f7f7f7",
             }}
@@ -50,11 +68,75 @@ function About() {
 
       <div style={{ position: "relative" }}>
         <div className="descrip-2">
-          <p>
-            See what app store reviews are about without lifting a finger. Make
-            better, faster decisions about your product roadmap, based on the
-            feedback and sentiment of real users.
-          </p>
+          <div class="rowCard">
+            <div class="columnCard">
+              <div class="card">
+                <h3 style={{ fontWeight: 700, paddingBottom: 20 }}>
+                  Bug Fixes
+                </h3>
+                <p
+                  style={{
+                    color: "black",
+                    textAlign: "justify",
+                    fontSize: "1vw",
+                  }}
+                >
+                  Mobile app users will leave reviews regarding all the bugs
+                  they encounter during their application experience. When ARC
+                  anaylses the reviews, the reviews addressing big fixes will be
+                  captured and displayed in a seperate page. You can analyse
+                  them and easily identify what bug fixes are mentioned the most
+                  and address the situation by implementing a solution to the
+                  bug.
+                </p>
+              </div>
+            </div>
+            <div class="columnCard">
+              <div class="card">
+                <h3 style={{ fontWeight: 700, paddingBottom: 20 }}>
+                  Feature Requests
+                </h3>
+                <p
+                  style={{
+                    color: "black",
+                    textAlign: "justify",
+                    fontSize: "1vw",
+                  }}
+                >
+                  Mobile app users will leave reviews regarding all the features
+                  they wish to have during their application experience. When
+                  ARC anaylses the reviews, the reviews addressing feature
+                  requests will be captured and displayed in a seperate page.
+                  You can analyse them and easily identify what features are
+                  requested the most and address the situation by implementing
+                  those features.
+                </p>
+              </div>
+            </div>
+            <div class="columnCard">
+              <div class="card">
+                <h3 style={{ fontWeight: 700, paddingBottom: 20 }}>
+                  Overall Sentiment
+                </h3>
+                <p
+                  style={{
+                    color: "black",
+                    textAlign: "justify",
+                    fontSize: "1vw",
+                  }}
+                >
+                  Mobile app users leave reviews about their mobile application
+                  experience in the Google Play Store. These reviews can either
+                  be positive or negative depending on their experience. ARC
+                  analyses all the reviews available for the application and
+                  uses cutting edge technology and sentiment analysis to
+                  calculate the overall sentiment of the mobile application. It
+                  will show whether the app has a positive or negative outlook
+                  for your users.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
@@ -63,7 +145,7 @@ function About() {
           <span
             class="border"
             style={{
-              backgroundColor: "transparent",
+              backgroundColor: "#282e34",
               fontSize: "2vw",
               color: "#f7f7f7",
             }}
@@ -75,20 +157,70 @@ function About() {
 
       <div style={{ position: "relative" }}>
         <div className="descrip-3">
-          <p>
+          <h3 style={{ textAlign: "center", fontSize: "2vw", fontWeight: 900 }}>
+            Make better and faster decisions
+          </h3>
+          <p
+            style={{
+              paddingLeft: 250,
+              paddingRight: 250,
+              paddingBlockStart: 30,
+            }}
+          >
             ARC provides fully automated, easy-to-understand sentiment analysis
             for mobile app reviews. Measure trends in user sentiment, review
             volume and star rating over time so you can see what users think of
-            changes you make to your apps.
+            changes you make to your apps. See what app store reviews are about
+            without lifting a finger. Make better, faster decisions about your
+            product roadmap, based on the feedback and sentiment of real users.
           </p>
         </div>
       </div>
 
       <div class="bgimg-1">
         <div class="caption">
-          <span class="border">Unlock your data with ARC!</span>
+          <span class="border">Unlock your data right now with ARC!</span>
         </div>
       </div>
+
+      <footer class="page-footer font-small blue pt-4">
+        <div class="container-fluid text-center text-md-left">
+          <div class="row">
+            <div class="col-md-6 mt-md-0 mt-3">
+              <h5 class="text-uppercase">Application Review Classifier</h5>
+              <p>
+                Your one stop for real time application review analysis.
+              </p>
+            </div>
+
+            <hr class="clearfix w-100 d-md-none pb-3">
+              <div class="col-md-3 mb-md-0 mb-3">
+                <h5 class="text-uppercase">Links</h5>
+
+                <ul class="list-unstyled">
+                  <li>
+                    <a href="#!">Home</a>
+                  </li>
+                  <li>
+                    <a href="#!">About</a>
+                  </li>
+                  <li>
+                    <a href="#!">Contact</a>
+                  </li>
+                  <li>
+                    <a href="#!">Support</a>
+                  </li>
+                </ul>
+              </div>
+            </hr>
+          </div>
+
+          <div class="footer-copyright text-center py-3">
+            © 2020 Copyright:
+            <a href="!#"> applicationreviewclassifier.com</a>
+          </div>
+        </div>
+      </footer>
     </Container>
   );
 }
