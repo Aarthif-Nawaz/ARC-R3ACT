8d8d3975a8cc06eea790e038d96a5444e165f706
ridmi98 <ridmi.2018182@iit.ac.lk>
2020-04-05 23:42:45 +0530

    modified ViewReviews.js

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,44 +1,29 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Footer from "../NavigationBar/Footer";
+
+
 import "../../App.css";
 
 function Review(props) {
   return (
-    <Container fluid>
-      {/* <p>Author Name : {props.author}</p>
-            <p>Date : {props.date}</p>
-            
+    <div className='Container-fluid'>
+      
+     
+
+      <div style={{ position: "relative" }}>
+        <div className="descrip-5 my-5 p-2">
+     
+            <p>Author Name:  {props.author}</p>
+            <p>Date: {props.date}</p>
             <div className="star">{[...Array(Number(props.score))].map(( i =>(
                 <label key={i+1}>★</label>
             )))} 
             </div>
-            <p>{props.text}</p> */}
-      <div class="bgimg-5">
-        <div class="caption">
-          <span className="border">
-            Reviews addressing "High Energy Consumption" ...
-          </span>
+            <p>Text:{props.text}</p>
+          
         </div>
       </div>
 
-      <div style={{ position: "relative" }}>
-        <div className="descrip-5">
-          <div
-            style={{
-              paddingLeft: 150,
-              paddingRight: 150,
-              paddingBlockStart: 10,
-            }}
-          >
-            <p>Author Name:</p>
-            <p>Date:</p>
-            <p>Text:</p>
-          </div>
-        </div>
-      </div>
-
-      <div style={{ position: "relative" }}>
+      {/* <div style={{ position: "relative" }}>
         <div className="descrip-4">
           <div
             style={{
@@ -100,10 +85,10 @@ function Review(props) {
             <p>Text:</p>
           </div>
         </div>
-      </div>
+      </div> */}
 
-      <Footer />
-    </Container>
+      
+    </div>
   );
 }
 export default Review;
