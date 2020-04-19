6dd971a279142b3e6d4f833905302344a766974b
Aarthif Nawaz <58284284+Aarthif-Nawaz@users.noreply.github.com>
2020-04-19 14:39:33 +0530

    Update TextPreProcessing.py

--- TextPreProcessing.py
+++ TextPreProcessing.py
@@ -93,13 +93,7 @@ def expand_contractions(phrase):
     phrase = re.sub(r"can\'t", "can not", phrase)
     # general
     phrase = re.sub(r"n\'t", " not", phrase)
-    phrase = re.sub(r"\'re", " are", phrase)
-    phrase = re.sub(r"\'s", " is", phrase)
-    phrase = re.sub(r"\'d", " would", phrase)
-    phrase = re.sub(r"\'ll", " will", phrase)
     phrase = re.sub(r"\'t", " not", phrase)
-    phrase = re.sub(r"\'ve", " have", phrase)
-    phrase = re.sub(r"\'m", " am", phrase)
     return phrase
 
 
