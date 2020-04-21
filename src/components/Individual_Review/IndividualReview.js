6b2890440c0dc3303422551fbf35721fe6441fc5
christina-thambirajah <58282529+christina-thambirajah@users.noreply.github.com>
2020-04-21 11:14:13 +0530

    individualReview fixed

--- src/components/Individual_Review/IndividualReview.js
+++ src/components/Individual_Review/IndividualReview.js
@@ -6,7 +6,7 @@ function IndividualReview() {
   return (
     <div className="container-fluid">
       <div class="bgimg-17">
-        <div className=" col-9 MenuBoxPage">
+        <div className=" col MenuBoxPage">
           <h3
             style={{
               textAlign: "center",
@@ -23,11 +23,11 @@ function IndividualReview() {
           <div className="reviewBox">
             <h3 style={{ fontWeight: 600 }}>Author name</h3>
             <h3 style={{ fontWeight: 500 }}>01/01/2020</h3>
-            <p style={{fontSize:"2vw"}}>Version: 2.20.108</p>
+            <p style={{fontSize:"1vw",marginBottom: "1vw"}}>Version: 2.20.108</p>
             <div className="star" style={{ color: "#000", marginBottom: "3%" }}>
               <label>★★★★★</label>
             </div>
-            <div variant="secondary" className="m-1 individualKeywordBtn">
+            <div variant="secondary" className=" individualKeywordBtn">
               video
             </div>
             <p className="reviewText" style={{ fontSize: "1.1vw" }}>
