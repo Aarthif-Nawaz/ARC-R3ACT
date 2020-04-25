ff58b219b65dc3eb450fb22e86b7c3c3c78ee983
christina-thambirajah <58282529+christina-thambirajah@users.noreply.github.com>
2020-04-25 18:14:50 +0530

    errors

--- Server_side/app.js
+++ Server_side/app.js
@@ -14,7 +14,6 @@ const path = require('path');
 
 var createError = require("http-errors");
 var express = require("express");
-var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var cors = require("cors");
