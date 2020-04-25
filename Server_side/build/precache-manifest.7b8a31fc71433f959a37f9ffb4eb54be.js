859ae04c872325cf2ce67cb801dac565a00fe101
christina-thambirajah <58282529+christina-thambirajah@users.noreply.github.com>
2020-04-25 20:46:06 +0530

    Build moved to server

---
+++ Server_side/build/precache-manifest.7b8a31fc71433f959a37f9ffb4eb54be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ebbe37fdad520f650649965b18586df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "618d7cf76a959aaa0787",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0db426b80ac106d6d6b6",
+    "url": "/static/css/main.06f750e5.chunk.css"
+  },
+  {
+    "revision": "618d7cf76a959aaa0787",
+    "url": "/static/js/2.37e62d77.chunk.js"
+  },
+  {
+    "revision": "e37bd506aa2f3a521ca43548db1eb314",
+    "url": "/static/js/2.37e62d77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0db426b80ac106d6d6b6",
+    "url": "/static/js/main.d46762f0.chunk.js"
+  },
+  {
+    "revision": "fa2ea1ea693f51a9bb7c",
+    "url": "/static/js/runtime-main.821acde1.js"
+  }
+]);
