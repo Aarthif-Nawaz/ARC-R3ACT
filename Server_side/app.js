c9f6b0f8df84a7fabe52044f4b8a0b3092776b6c
Shiromi <basilanita@gmail.com>
2020-04-26 20:29:14 +0530

    Added CORS settings

--- Server_side/app.js
+++ Server_side/app.js
@@ -59,7 +59,22 @@ app.use("/contactus", contactUsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+  // next(createError(404));
 });
 
 // error handler
