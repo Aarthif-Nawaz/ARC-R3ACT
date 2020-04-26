352e56ffe1dabdc577e3ece9e21d7ab88929d259
SafiyyahR <safiyyahrahman00@gmail.com>
2020-04-26 10:39:19 +0530

    added instance to the services

--- Server_side/services/appdetails.service.js
+++ Server_side/services/appdetails.service.js
@@ -11,12 +11,12 @@ const client = require("../db/mongo").client;
  * Connects to the mongodb Atlas database.
  */
 var db;
-client.connect((err) => {
+client.connect((err,instance) => {
   if (err) {
     // Displaying error message if an error occurs while connecting to the database
     console.log("Error has occured while connecting to database: ", err);
   }
-  db = client.db("ARC"); // Store the name of the database in a variable
+  db = instance.db("ARC"); // Store the name of the database in a variable
   // Displaying success message if the database connection is successful
   console.log("Connected to database - app");
   // client.close();
